c8303de02e855b6cb9d37c715d93fd0b076c9c17
Sevde Salkım <sevde.salkim@gmail.com>
2024-09-25 22:34:19 +0300

    Dizi tanımlama öğrenildi.

---
+++ giris/DizilereGiris/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizilereGiris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* İNT VERİ TİPİ İLE DİZİ TANIMLAMA
+            int[] sayilar = new int[4];
+            sayilar[0] = 1;
+            sayilar[1] = 2;
+            sayilar[2] = 3;
+            sayilar[3] = 4;
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+
+            }
+            Console.ReadLine();
+
+           
+            STRİNG VERİ TİPİ İLE DİZİ TANIMLAMA 
+            string[] isimler = new string[5];
+            isimler[0] = "Muhammed";
+            isimler[1] = "ve";
+            isimler[2] = "Sevde";
+            isimler[3] = "Birbirlerine";
+            isimler[4] = "Aşıklar";
+
+            for (int i = 0; i < isimler.Length; i++) {
+                Console.WriteLine(isimler[i]);
+            }
+            Console.ReadLine();
+            
+            DOUBLE VERİ TİPİ İLE DİZİ TANIMLAMA 
+
+            double[] ondalikli_sayilar = { 1.25, 1.56, 2.38, 0.12 };
+            for (int i = 0; i < ondalikli_sayilar.Length; i++)
+            {
+                Console.WriteLine(ondalikli_sayilar[i]);
+            }
+            Console.ReadLine();
+            
+            CHAR VERİ TİPİ İLE  DİZİ TANIMLAMA
+
+            char[] karakter = { 'M', 'U', 'H', 'A', 'M', 'M', 'E', 'D' };
+
+            for (int i = 0; i < karakter.Length; i++)
+            {
+                Console.WriteLine(karakter[i]);
+            }
+            Console.ReadLine();
+
+            */
+        }
+    }
+}
---
+++ giris/DizilereGiris/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DizilereGiris")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DizilereGiris")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c0ea037-5d1d-4b43-a5d6-5d40165799ae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
