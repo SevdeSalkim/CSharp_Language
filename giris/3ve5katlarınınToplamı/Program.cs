014288eb25727dab83bf7137ac9d8f0e902bf935
Sevde Salkım <sevde.salkim@gmail.com>
2024-09-28 17:18:27 +0300

    3 ve 5 in katlarının toplamını ekrana yazdıran program

---
+++ giris/3ve5katlarınınToplamı/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ve5katlarınınToplamı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, i, sum = 0;
+
+            for (i = 1; i < 100; i++) {
+                a = i % 3;
+                b = i % 5;
+
+                if (a == 0 || b == 0)
+                {
+                    Console.Write("{0} \t ", i);
+                    sum = sum + i;
+                }
+            }
+            
+            Console.WriteLine(" \n 100'ün Altındaki 3 veya 5'in Katlarının Toplamı : {0}", sum);
+            Console.Read();
+        }
+    }
+}
