e05044e80a0fe68be5c5679a1f7ddfb9d689a2c8
Sevde Salkım <sevde.salkim@gmail.com>
2024-09-29 23:56:44 +0300

    method örneği yapıldı

---
+++ giris/MethodOrnek1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOrnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan aldığımız səyinin karesini alalım eğer karesi 25 'ten büyükse;
+            // karesi 25 ten büyüktür
+            // değilse 25 'ten küçüktür yazdırsın.
+
+
+            Console.Write("Lütfen bir sayı giriniz.");
+
+            int sayi = int.Parse(Console.ReadLine());
+
+            int kareDeger = kareAl(sayi);
+
+            if(kareDeger > 25)
+            {
+                Console.WriteLine("Girilen sayının karesi 25 ten büyüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayının karesi 25ten küçüktür");
+            }
+            Console.ReadLine(); 
+
+        }
+
+        static int kareAl(int i) {
+            return i * i;
+        }
+    }
+}
