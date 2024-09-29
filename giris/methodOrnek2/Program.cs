3b1a6875c0a3053687167d8449ae6e1eabfdade1
Sevde Salkım <sevde.salkim@gmail.com>
2024-09-30 00:59:03 +0300

    girilen sayının tek mi çift mi olduğunu bulan program

---
+++ giris/methodOrnek2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodOrnek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+
+            int girilenSayi = int.Parse(Console.ReadLine());
+
+            tekmiCiftmi(girilenSayi);
+
+            Console.ReadLine();
+
+        }
+
+        static void tekmiCiftmi(int sayi)
+        {
+            if (sayi %2 == 0)
+            {
+                Console.WriteLine("Girilen sayı çifttir");
+            }
+            else{
+                Console.WriteLine("Girilen sayı tektir.");
+                
+            }
+        }
+    }
+}
