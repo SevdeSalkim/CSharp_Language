7c1510ad8a389eb5dc43569b3bcd511ee08f856f
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-02 01:26:33 +0300

    classlara giriş yapıldı.

---
+++ giris/ClassKullanımı/Araba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKullanımı
+{
+    public class Araba
+    {
+        public int kapiSayisi;
+        public string arabaModeli;
+
+        public string arabaRengi;
+
+
+
+        public void motorCalistir()
+        {
+            Console.WriteLine("Motor Çalışıyor");
+        }
+
+        public void kapılarıKilitle()
+        {
+            Console.WriteLine("Kapılar Kilitleniyor...");
+        }
+    }
+}
---
+++ giris/ClassKullanımı/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassKullanımı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba();
+
+            araba1.arabaRengi = "kırmızı";
+            araba1.arabaModeli = "toyota";
+            araba1.kapiSayisi = 4;
+
+            araba1.kapılarıKilitle();
+
+            Console.WriteLine("Arabanın Rengi: " + araba1.arabaRengi);
+            Console.WriteLine("Arabanın Modeli: " + araba1.arabaModeli);
+
+            Console.ReadLine();
+        }
+    }
+}
