89ecad85cd73234390d0817a98859da175ca67d5
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-07 22:56:33 +0300

    Rectangle Example

---
+++ giris/RectangleClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle rect1 = new Rectangle(5, 12);
+            Rectangle rect2 = new Rectangle(7,14);
+
+            
+
+            Console.WriteLine($"Rectangle1 Area: " + rect1.GetArea() + " " + "Rectangle1 Perimeter: " + rect1.GetPerimeter());
+            Console.WriteLine($"Rectangle2 Area: " + rect2.GetArea()+ " " + "Rectangle2 Perimeter: " + rect2.GetPerimeter());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ giris/RectangleClass/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleClass
+{
+    internal class Rectangle
+    {
+        //Alan ve çevre hesaplamak için GetArea() ve GetPerimeter() metodlarını yaz.
+        //  Main metodunda birkaç dikdörtgen oluştur ve alanlarını/çevrelerini ekrana yazdır.
+
+        private int Width;
+        private int Height;
+
+        public Rectangle(int _width, int _height)
+        {
+           this.Width = _width;
+           this.Height = _height;  
+        }
+
+        public double GetArea()
+        {
+            double area =  Width * Height;
+            return area;
+        }
+
+        public double GetPerimeter()
+        {
+            double perimeter = 2 * (Width + Height);
+            return perimeter;
+        }
+    }
+}
