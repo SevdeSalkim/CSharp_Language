e0811c4f30797b77c7cda5a9792ca460d191b6b7
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-07 21:55:33 +0300

    Class kullanlarak örnek yapıldı.

---
+++ giris/OgrenciBilgiSistemi/Ogrenci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciBilgiSistemi
+{
+    public class Ogrenci
+    {
+        private int ogrenciNo;
+        private string ogrenciİsim;
+        private string ogrenciSoyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulIsim;
+
+        public Ogrenci(int _ogrenciNo, string _ogrenciİsim, string _ogrenciSoyisim, int _vize1, int _vize2, int _final, string _okulIsim)
+        {
+            this.ogrenciNo = _ogrenciNo;
+            this.ogrenciİsim = _ogrenciİsim;
+            this.ogrenciSoyisim = _ogrenciSoyisim;
+            this.vize1 = _vize1;
+            this.vize2 = _vize2;
+            this.final = _final;
+            this.okulIsim = _okulIsim;
+        }
+
+        public void ogrenciBilgileri()
+        {
+            Console.WriteLine("Öğrenci numarası: " + ogrenciNo);
+            Console.WriteLine("Öğrenci AD ve SOYAD: " + ogrenciİsim + " " + ogrenciSoyisim);
+            Console.WriteLine("Vİze 1 notu: " + vize1);
+            Console.WriteLine("Vize2 notu: " + vize2);
+            Console.WriteLine("Final notu: " + final);
+            
+        }
+
+        public double ogrenciOrtalaması()
+        {
+            double ortalama = ((vize1 * 0.2) + (vize2 * 0.2) + final * 0.6);
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Okul Bilgisi: " + okulIsim);
+
+        }
+    }
+}
---
+++ giris/OgrenciBilgiSistemi/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciBilgiSistemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogrenci1 = new Ogrenci(1,"Sevde","Salkim",25,45,80,"KBÜ");
+
+            Console.WriteLine("Uygulamamıza Hoşgeldiniz. Yapmak İstediğiniz işlemi seçiniz.");
+            
+
+            while (kontrol) {
+                islemleriGöster();
+                string secim = Console.ReadLine();
+                
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.ogrenciBilgileri();
+                        break;
+
+                    case "2":
+                        double ogrenciOrtalama =  ogrenci1.ogrenciOrtalaması();
+                        Console.WriteLine(ogrenciOrtalama);
+                        break;
+                    case "3":
+                        ogrenci1.okulGetir();
+                        break;
+
+                    case "4":
+                        kontrol = false;
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Geçersiz seçim, lütfen tekrar deneyin.");
+                        break;
+
+                }
+
+
+            }
+
+        }
+
+        static void islemleriGöster()
+        {
+            Console.WriteLine("1.ÖğrenciBilgileri göster.");
+            Console.WriteLine("2.Öğrenci ortalamasını göster.");
+            Console.WriteLine("3.öğrenci okulunu göster.");
+            Console.WriteLine("4.Çıkış yap");
+
+        }
+    }
+}
