9c86ce32d56f9dbac14fbc06c1b09cef2e9c9b95
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-07 21:54:27 +0300

    Erişim Belirliyiciler öğrenildi.

---
+++ giris/ErisimBelirleyici/Musteri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyici
+{
+    internal class Musteri
+    {
+        public string isim;
+        public string soyisim;
+        public int maasDegeri;
+        public string cinsiyet;
+        private int yas;
+
+
+
+        public void MusteriBilgileriGoster()
+        {
+            Console.WriteLine("İsmi: " + isim);
+            Console.WriteLine("Soyismi: " + soyisim);
+            Console.WriteLine("Maaş Bilgisi: " + maasDegeri);
+            Console.WriteLine("Cinsiyet: " + cinsiyet);
+            Console.WriteLine("Yaş: " + yas);
+        }
+
+        private void IsimSoyisimYazdır(string musteriIsim, string musteriSoyisim)
+        {
+            Console.WriteLine("Müşteri İsim ve Soyismi: " + musteriIsim + " " + musteriSoyisim);
+        }
+    }
+}
---
+++ giris/ErisimBelirleyici/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyici
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //public -- private
+
+            Musteri musteri1 = new Musteri();
+
+            musteri1.isim = "Sevde";
+            musteri1.soyisim = "Salkim";
+            musteri1.maasDegeri = 20000;
+            musteri1.cinsiyet = "Kadın";
+
+        }
+    }
+}
