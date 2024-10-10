e2e2a6e7b998b17957ba809a2528a8de6ec5f14e
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-11 00:18:00 +0300

    encapsulation konusuna giriş yapıldı.

---
+++ giris/Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci();
+
+            ogrenci1.setisim("Sevde");
+            ogrenci1.getisim();
+            Console.ReadLine();
+        }
+    }
+}
