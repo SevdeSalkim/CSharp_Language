01d85b95d2c9db13d66a88b18f93683fe1facefb
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-14 23:45:41 +0300

    kapsulleme örnek yapıldı

---
+++ giris/KapsullemeOrnek/Personel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeOrnek
+{
+    public class Personel
+    {
+        private string tcKimlikNo;
+
+        public string TCKİMLİKNO {
+            
+            get {
+                return tcKimlikNo.Substring(0, 4);
+
+            } 
+            
+            set {
+                bool control = false;
+                if (value.Length == 11)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool sayimi = char.IsNumber(value[i]);
+
+                        if (sayimi) { 
+                            // 
+                        }
+                        else
+                        {
+                            control = true;
+                            break;
+                        }
+                    }
+                    if (control) {
+                        Console.WriteLine("lütfen tc kimlik numaranızı geçersiz karakter bulunmaktadır.");
+                    }
+                    else
+                    {
+                        tcKimlikNo = value;
+                    }
+                    
+
+                }
+                else {
+                    Console.WriteLine("Girdiğiniz kimlik numarası 11 karakterli değildir.");                
+                }
+            } 
+        }
+    }
+}
---
+++ giris/KapsullemeOrnek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel personel1 = new Personel();
+
+            personel1.TCKİMLİKNO = "12345678945";
+
+            Console.WriteLine( "girdiğiniz tc kimlik numaranız: " + personel1.TCKİMLİKNO);
+            Console.ReadLine();
+        }
+    }
+}
