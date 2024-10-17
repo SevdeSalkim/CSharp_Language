32fafbab00630dc379f4dc53b33ed3eae7ee3540
Sevde Salkım <sevde.salkim@gmail.com>
2024-10-18 01:27:44 +0300

    Statik method kullanımı

---
+++ giris/StaticMethod/Personel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    public class Personel
+    {
+        public static void isimYazdir() {
+            Console.WriteLine("Sevde Salkım");
+
+        }
+
+        public static void toplamYazdir(int sayi1, int sayi2) {
+            Console.WriteLine("iki sayının toplamı : " + (sayi1 + sayi2));
+            
+        }
+    }
+}
---
+++ giris/StaticMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Personel.isimYazdir();
+            // Personel.toplamYazdir(5, 8);
+
+            Calisan C1 = new Calisan();
+
+            C1.calisanID = 1;
+            C1.isim = "Sevde";
+            C1.soyisim = "Salkım";
+
+            Calisan.maas = 25000;
+
+            Console.WriteLine("Çalışanın ID: " + C1.calisanID);
+            Console.WriteLine("Çalışan İsim: " + C1.isim + " " + C1.soyisim);
+            Console.WriteLine("Çalışan Maaşı: " + Calisan.maas);
+            Console.ReadLine();
+        }
+    }
+}
